c91e241d893701503ac94444a0599ea3d7b87c4a
Kerstin Cramer Kerstin.Cramer@uni.lu <kcramer@access.gaia-cluster.uni.lux>
2017-11-14 16:43:42 +0100

    corrected

--- solvers/mat_vec_bnd.py
+++ solvers/mat_vec_bnd.py
@@ -50,36 +50,29 @@ def mat_vec_bnd(a, phi):
     r[:] -= a.T[:] * cat_z( (phi.val       [:,:, 1:], 
                              phi.bnd[T].val[:,:, :1]) )
 
-
-
     if True:
         # append x-boundary conditions to phi
         phi_gpu = cat_x(( phi.bnd[W].val[:1,:,:], phi.val[:,:,:], phi.bnd[E].val[:1,:,:] ))
         
         # append y-boundary conditions to phi
-        y_bnd_N = cat_x(( zeros(( 1,1,shape(phi.bnd[N].val)[2] )), phi.bnd[N].val, zeros(( 1,1,shape(phi.bnd[N].val)[2] )) ))
-        y_bnd_S = cat_x(( zeros(( 1,1,shape(phi.bnd[S].val)[2] )), phi.bnd[S].val, zeros(( 1,1,shape(phi.bnd[S].val)[2] )) ))
+        y_bnd_N = pad( phi.bnd[N].val, ((1, 1), (0, 0), (0, 0)), 'constant' ) 
+        y_bnd_S = pad( phi.bnd[S].val, ((1, 1), (0, 0), (0, 0)), 'constant' )
         phi_gpu = cat_y(( y_bnd_S, phi_gpu, y_bnd_N ))
 
         # append z-boundary condtiions to phi
-        z_bnd_B = cat_x(( zeros(( 1,shape(phi.bnd[B].val)[1],1)), phi.bnd[B].val, zeros((1,shape(phi.bnd[B].val)[1],1)) ))
-        z_bnd_B = cat_y(( zeros(( shape(z_bnd_B)[0],1,1 )), z_bnd_B, zeros(( shape(z_bnd_B)[0],1,1 )) ))
-        z_bnd_T = cat_x(( zeros(( 1,shape(phi.bnd[T].val)[1],1 )), phi.bnd[T].val, zeros(( 1,shape(phi.bnd[T].val)[1],1 )) ))
-        z_bnd_T = cat_y(( zeros(( shape(z_bnd_T)[0],1,1 )), z_bnd_T, zeros(( shape(z_bnd_T)[0],1,1 )) ))
+        z_bnd_B = pad( phi.bnd[B].val, ((1, 1), (1, 1), (0, 0)), 'constant' )
+        z_bnd_T = pad( phi.bnd[T].val, ((1, 1), (1, 1), (0, 0)), 'constant' )
         phi_gpu = cat_z(( z_bnd_B, phi_gpu, z_bnd_T ))
         
-        print(shape(phi_gpu))
-   
-    # preparing a for gpu
-    if True:
+        # preparing a for gpu
         a_C_gpu = pad(a.C, ((1, 1), (1, 1), (1, 1)), 'constant')
-        a_W_gpu = pad(a.W, ((1, 1), (0, 2), (1, 1)), 'constant')
-        a_E_gpu = pad(a.E, ((1, 1), (2, 0), (1, 1)), 'constant')
-        a_S_gpu = pad(a.S, ((1, 1), (1, 1), (0, 2)), 'constant')
-        a_N_gpu = pad(a.N, ((1, 1), (1, 1), (2, 0)), 'constant')
-        a_B_gpu = pad(a.B, ((2, 0), (1, 1), (1, 1)), 'constant')
-        a_T_gpu = pad(a.T, ((0, 2), (1, 1), (1, 1)), 'constant')
-        print(shape(a_C_gpu))
+        a_W_gpu = pad(a.W, ((0, 2), (1, 1), (1, 1)), 'constant')
+        a_E_gpu = pad(a.E, ((2, 0), (1, 1), (1, 1)), 'constant')
+        a_S_gpu = pad(a.S, ((1, 1), (0, 2), (1, 1)), 'constant')
+        a_N_gpu = pad(a.N, ((1, 1), (2, 0), (1, 1)), 'constant')
+        a_B_gpu = pad(a.B, ((1, 1), (1, 1), (0, 2)), 'constant')
+        a_T_gpu = pad(a.T, ((1, 1), (1, 1), (2, 0)), 'constant')
 
+       
 
     return r  # end of function
