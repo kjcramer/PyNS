2832476d3934d5deceec44889fd41cbbf6a0401d
Kerstin Cramer Kerstin.Cramer@uni.lu <kcramer@access.gaia-cluster.uni.lux>
2017-11-21 11:52:54 +0100

    fix typos, now it runs!

--- solvers/mat_vec_bnd.py
+++ solvers/mat_vec_bnd.py
@@ -40,7 +40,7 @@ def mat_vec_bnd(a, phi, gpu=False):
         phi_gpu = phi
 
         r_gpu = gpuarray.zeros_like(phi_gpu.val)
-        x_gpu = gpuarray.zeros_like(phi_gpu.val)        
+        x_gpu = gpuarray.zeros_like(phi_gpu.val)
       
         r_gpu = a_gpu.C * phi_gpu.val
     
@@ -48,9 +48,9 @@ def mat_vec_bnd(a, phi, gpu=False):
         x_gpu[1:,:,:]  = phi_gpu.val       [:-1,:,:]
         r_gpu = r_gpu - a_gpu.W * x_gpu
 
-        print(type(phi_gpu.val[1:,:,:]), type(x_gpu[:-1,:,:]))    
-        print(shape(phi_gpu.val[1:,:,:]), shape(x_gpu[:-1,:,:]))    
-        x_gpu[:-1,:,:] = phi_gpu.val[ 1:,:,:],
+        # print(type(phi_gpu.val[1:,:,:]), type(x_gpu[:-1,:,:]))    
+        # print(shape(phi_gpu.val[1:,:,:]), shape(x_gpu[:-1,:,:]))    
+        x_gpu[:-1,:,:] = phi_gpu.val[ 1:,:,:]
         x_gpu[-1:,:,:] = phi_gpu.bnd[E].val[ :1,:,:]
         r_gpu = r_gpu - a_gpu.E * x_gpu
     
@@ -62,11 +62,11 @@ def mat_vec_bnd(a, phi, gpu=False):
         x_gpu[:,-1:,:] = phi_gpu.bnd[N].val[:, :1,:]
         r_gpu = r_gpu - a_gpu.N * x_gpu
         
-        x_gpu[:,:,:1]  = phi_gpu.bnd[B].val[:,:, :1], 
+        x_gpu[:,:,:1]  = phi_gpu.bnd[B].val[:,:, :1]
         x_gpu[:,:,1:]  = phi_gpu.val[:,:,:-1]
         r_gpu = r_gpu - a_gpu.B * x_gpu
     
-        x_gpu[:,:,:-1] = phi_gpu.val[:,:, 1:], 
+        x_gpu[:,:,:-1] = phi_gpu.val[:,:, 1:] 
         x_gpu[:,:,-1:] = phi_gpu.bnd[T].val[:,:, :1]
         r_gpu = r_gpu - a_gpu.T * x_gpu
     
