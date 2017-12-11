01e21880e59eed3bbbd78cf1540a9d043b9ccab4
Kerstin Cramer Kerstin.Cramer@uni.lu <kcramer@node-182.gaia-cluster.uni.lux>
2017-12-11 15:51:25 +0100

    first gpu results from gaia

--- pycuda_dev/pycuda_thinner_collocated.py
+++ pycuda_dev/pycuda_thinner_collocated.py
@@ -53,8 +53,8 @@ def main(show_plot=True, time_steps=160, plot_freq=20):
 
     # Node coordinates -- 128x32x32 was original mesh size
     xn = nodes(0, 1.25, 128)
-    yn = nodes(0, 0.125, 16)
-    zn = nodes(0, 0.125, 16)
+    yn = nodes(0, 0.125, 32)
+    zn = nodes(0, 0.125, 32)
 
     # Cell coordinates
     xc = avg(xn)
--- pycuda_dev/run_tests.sh
+++ pycuda_dev/run_tests.sh
@@ -2,16 +2,16 @@
 # dirty but effective
 
 python pycuda_thinner_collocated.py > out.txt
-mkdir gpu_gaia_128_16_16_run1
-mv fig* gpu_gaia_128_16_16_run1
-mv out.txt gpu_gaia_128_16_16_run1
+mkdir gpu_gaia_128_32_32_run1
+mv fig* gpu_gaia_128_32_32_run1
+mv out.txt gpu_gaia_128_32_32_run1
 
 python pycuda_thinner_collocated.py > out.txt
-mkdir gpu_gaia_128_16_16_run2
-mv fig* gpu_gaia_128_16_16_run2
-mv out.txt gpu_gaia_128_16_16_run2
+mkdir gpu_gaia_128_32_32_run2
+mv fig* gpu_gaia_128_32_32_run2
+mv out.txt gpu_gaia_128_32_32_run2
 
 python pycuda_thinner_collocated.py > out.txt
-mkdir gpu_gaia_128_16_16_run3
-mv fig* gpu_gaia_128_16_16_run3
-mv out.txt gpu_gaia_128_16_16_run3
+mkdir gpu_gaia_128_32_32_run3
+mv fig* gpu_gaia_128_32_32_run3
+mv out.txt gpu_gaia_128_32_32_run3
