f4f61955bf45ad42f83896a3f70a4db62932a520
Luca Manzari <manzariluca@gmail.com>
2017-12-15 15:40:39 +0100

    tests on gaia, code is broken

--- pycuda_dev/pycuda_thinner_collocated.py
+++ pycuda_dev/pycuda_thinner_collocated.py
@@ -52,9 +52,9 @@ def main(show_plot=True, time_steps=100, plot_freq=20):
 # =============================================================================
 
     # Node coordinates -- 128x32x32 was original mesh size
-    xn = nodes(0, 1.25, 128)
-    yn = nodes(0, 0.125, 32)
-    zn = nodes(0, 0.125, 32)
+    xn = nodes(0, 1.25, 256)
+    yn = nodes(0, 0.125, 64)
+    zn = nodes(0, 0.125, 64)
 
     # Cell coordinates
     xc = avg(xn)
