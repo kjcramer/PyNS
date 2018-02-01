e00a0573b1172ba344423ea8b77cc5cf8f2b3ab9
Luca Manzari <manzariluca@gmail.com>
2018-02-01 17:32:48 +0100

    consistent code and style on figure 3, benchmarks

--- pycuda/benchmarks/benchmark003_plotting.py
+++ pycuda/benchmarks/benchmark003_plotting.py
@@ -1,20 +1,10 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 30 13:29:44 2015
-
-@author: kerstin.cramer
-"""
-
 import numpy as np
+import matplotlib
 import matplotlib.pylab as pylab
 import matplotlib.pyplot as plt
-import matplotlib
-
 
 data = np.load('benchmark003_outData_gaia_k80.npz')
 fig_name = 'benchmark003_outData_gaia_K80.eps'
-
 
 times = data['times']
 iters = data['iterations']
@@ -23,33 +13,39 @@ size = data['size']
 iteration = 0;
 siz = 7;
 
+# cosmetics
+main_text_size = 12
+title_size = 14
+legend_size = 10
 
+# figure
 fig, (ax1,ax2) = plt.subplots(2,1)
+
+# top figure
 ax1.plot(size,times[0,:,iteration],label="CPU",linewidth=1.5)
 ax1.plot(size,times[1,:,iteration],label="gpuarray",linewidth=1.5)
 ax1.plot(size,times[2,:,iteration],label="redKernel",linewidth=1.5)
-ax1.legend(loc=2,fontsize=14)
-##plt.yscale('log');
-ax1.set_xlabel('Matrix Size',fontsize=14)
+
+ax1.legend(loc=2,fontsize=legend_size)
+ax1.set_xlabel('Matrix Size', fontsize=main_text_size)
+ax1.set_ylabel('Elapsed Time [s]', fontsize=main_text_size)
+ax1.set_title('All Matrix Sizes', fontsize=title_size)
+ax1.tick_params(axis='both', which='major', labelsize=main_text_size)
 ax1.set_xlim([50,525]);
-ax1.set_ylabel('Elapsed Time [s]' , fontsize=14)
-#ax1.set_title('Iteration = ' + str(iters[iteration]), fontsize= 14)
-matplotlib.rcParams.update({'font.size': 14})
-matplotlib.rc('ytick', labelsize=14)
 
-
+# bottom figure
 ax2.plot(size,times[0,:,iteration],label="CPU",linewidth=1.5)
 ax2.plot(size,times[1,:,iteration],label="gpuarray",linewidth=1.5)
 ax2.plot(size,times[2,:,iteration],label="redKernel",linewidth=1.5)
-ax2.legend(loc=2,fontsize=14)
-#plt.yscale('log');
-ax2.set_xlabel('Matrix Size',fontsize=14)
+
+ax2.legend(loc=2,fontsize=legend_size)
+ax2.set_xlabel('Matrix Size', fontsize=main_text_size)
+ax2.set_ylabel('Elapsed Time [s]',fontsize=main_text_size)
+ax2.set_title('Magnified View', fontsize=title_size)
+ax2.tick_params(axis='both', which='major', labelsize=main_text_size)
 ax2.set_xlim([50,210])
 ax2.set_ylim([0,0.05])
-ax2.set_ylabel('Elapsed Time [s]' , fontsize=14)
-##plt.figtext(0,1,title,fontsize='11', bbox=dict(facecolor='none', edgecolor='black', pad=10.0));
-##plt.figtext(0,1.1, Mass_Flux_str,fontsize='9', bbox=dict(facecolor='none', edgecolor='black', pad=10.0));
-plt.tight_layout();
 
+# global tweaks and exports
+plt.tight_layout();
 pylab.savefig(fig_name, dpi=100,bbox_inches='tight');
-pylab.show()
