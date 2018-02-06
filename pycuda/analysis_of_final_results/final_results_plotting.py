16a894c4d1b581ea7a9f6ff88c0edfd8ea5451f0
Luca Manzari <manzariluca@gmail.com>
2018-02-06 16:37:14 +0100

    very very very final results with plots

--- pycuda/analysis_of_final_results/final_results_plotting.py
+++ pycuda/analysis_of_final_results/final_results_plotting.py
@@ -56,7 +56,7 @@ legend_size = 10
 size=np.arange(len(mesh_sizes))
 fig_name_1='Comparison_time_per_iteration.png'
 
-# plotting average time per iteration 
+# plotting average time per iteration
 plt.ion()
 
 fig, (ax1,ax2) = plt.subplots(1,2)
@@ -66,6 +66,7 @@ ax1.plot(size+1,results_sum_tegner_cpu[6,:]/results_sum_tegner_cpu[7,:],'--',col
 ax1.plot(size+1,results_sum_tegner_gpu[6,:]/results_sum_tegner_gpu[7,:],'-',color='k',label="GPU Tegner",linewidth=1.5)
 ax1.legend(loc=2,fontsize=legend_size)
 ax1.set_xlabel('Mesh',fontsize=main_text_size)
+ax1.set_xticks([1,2,3,4,5,6,7])
 ax1.set_ylabel('Average Time per Iteration [s]' , fontsize=main_text_size)
 ax1.set_title('All Meshes',fontsize=title_size)
 ax1.tick_params(axis='both', which='major', labelsize=main_text_size)
@@ -88,7 +89,7 @@ pylab.savefig(fig_name_1, dpi=100,bbox_inches='tight');
 
 #%% plot final comparison plots total time
 
-fig_name_2='Comparison_total_time.png' 
+fig_name_2='Comparison_total_time.png'
 
 fig, (ax1,ax2) = plt.subplots(1,2)
 ax1.plot(size+1,results_gaia_cpu[8,199,:]/3600,'--',color='g',label="CPU Gaia",linewidth=1.5)
@@ -97,6 +98,7 @@ ax1.plot(size+1,results_tegner_cpu[8,199,:]/3600,'--',color='k',label="CPU Tegne
 ax1.plot(size+1,results_tegner_gpu[8,199,:]/3600,'-',color='k',label="GPU Tegner",linewidth=1.5)
 ax1.legend(loc=2,fontsize=legend_size)
 ax1.set_xlabel('Mesh',fontsize=main_text_size)
+ax1.set_xticks([1,2,3,4,5,6,7])
 ax1.set_ylabel('Elapsed Time [h]',fontsize=main_text_size )
 ax1.tick_params(axis='both', which='major', labelsize=main_text_size)
 ax1.set_title('All Meshes',fontsize=title_size)
@@ -120,4 +122,3 @@ pylab.savefig(fig_name_2, dpi=100,bbox_inches='tight');
 
 speed_gaia = results_gaia_cpu[8,199,:]/results_gaia_gpu[8,199,:]
 speed_tegner = results_tegner_cpu[8,199,:]/results_tegner_gpu[8,199,:]
-
