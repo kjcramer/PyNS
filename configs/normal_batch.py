fea5f613f5263fe35dde55bff28b0f148b69c6c3
Kerstin CRAMER <kerstin.cramer@uni.lu>
2018-11-26 16:16:54 +0100

    local changes

--- configs/normal_batch.py
+++ configs/normal_batch.py
@@ -7,9 +7,10 @@ from pyns.configs.normal_config_8 import *
 from pyns.configs.normal_config_2 import *
 from pyns.configs.normal_config_05 import *
 
+#%%
 u_h_in = [0.025, 0.05, 0.1]
 
 for uu in u_h_in:
-  normal_config_05(80, uu, 70000, False)
+  normal_config_2(70, uu, 70000, False)
   
   
