de4879de866d8b289bc567eec9078f44baa2046e
Kerstin Cramer <kerstin.cramer@uni.lu>
2019-01-23 10:47:33 +0100

    write aout salt infos

--- configs/reversed_batch.py
+++ configs/reversed_batch.py
@@ -15,4 +15,4 @@ from pyns.configs.reversed_config_salt_05 import *
 #u_h_in = [0.025, 0.05, 0.1]
 
 #for uu in u_h_in:
-reversed_config_8(80, 0.05, 100000, True)
+reversed_config_2(80, 0.05, 100000, True)
